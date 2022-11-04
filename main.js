327e06268b7cc738a5a1d81e9e2e837d8e571bae
akshaj-legorobot <84026684+akshaj-legorobot@users.noreply.github.com>
2022-11-04 16:55:39 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,11 @@ leftWristX = 0;
 leftWristY = 0;
 rightWristX = 0;
 rightWristY = 0;
+scoreLeftWrist = 0;
+scoreRightWrist = 0;
+song1_status = "";
+song2_status = "";
+
 
 
 function preload() {
@@ -15,13 +20,33 @@ function setup() {
     canvas = createCanvas(600, 500);
     canvas.center();
     video = createCapture(VIDEO);
-    vidoe.hide();
+    video.hide();
     poseNet = ml5.poseNet(video, modelLoaded);
     poseNet.on('pose', gotPoses);
 }
 
 function draw() {
     image(video, 0, 0, 600, 500);
+    song1_status = song1.isPlaying();
+    song2_status = song2.isPlaying();
+    fill("#FF0000");
+    stroke("#FF0000");
+    if (scoreRightWrist > 0.2) {
+        circle(rightWristX, rightWristY, 20);
+        song2.stop();
+        if (song1_status == false) {
+            song1.play();
+            document.getElementById("song").innerHTML = "playing - harryPotter theme song";
+        }
+        if (scoreLeftWrist > 0.2) {
+            circle(leftWristX, leftWristY, 20);
+            song1.stop();
+            if (song2_status == false) {
+                song2.play();
+                document.getElementById("song").innerHTML = "playing - peterpan song";
+            }
+        }
+    }
 }
 
 function modelLoaded() {
@@ -31,6 +56,8 @@ function modelLoaded() {
 function gotPoses(results) {
     if (results.length > 0) {
         console.log(results);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log("scoreLeftWrist = " + scoreLeftWrist);
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
         console.log("leftWristX = " + leftWristX + " leftWristY = " + leftWristY);
